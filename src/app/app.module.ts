ced11ab39cd8d0c66d9ebd99fb82b4f6be2a6cf9
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2023-01-10 16:54:11 -0700

    comienzo de rutas hijas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { PagesModule } from './pages/pages.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+
+
+import { AuthModule } from './auth/auth.module';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
 
 
 
@@ -15,17 +16,13 @@ import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component'
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    NopagefoundComponent,
-    
-    
+    NopagefoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PagesModule,
-   
+    AuthModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
