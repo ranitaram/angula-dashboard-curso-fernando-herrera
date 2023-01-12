f3bd8047948b7400302434120380abc8146b1927
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2023-01-11 17:33:46 -0700

    Uso de atributos personalizados

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule} from '@angular/forms'
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing.module';
+
 
 
 
@@ -19,7 +21,8 @@ import { AppRoutingModule } from '../app-routing.module';
   imports: [
     CommonModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   exports: [
     DashboardComponent,
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
   styleUrls: ['./progress.component.css']
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent  {
 
-  constructor() { }
+  progreso: number = 20;
 
-  ngOnInit(): void {
+  get getPorcentaje(){
+    return `${this.progreso}%`;
   }
 
+  cambiarValor(valor: number): 100 | 0 | undefined {
+    //validacion para que no pase mas de su rango y tengamos un bug
+
+    if(this.progreso >= 100 && valor >= 0){
+      return this.progreso = 100;
+    }
+
+    if (this.progreso <= 0 && valor < 0) {
+      return this.progreso = 0;
+    }
+
+    this.progreso = this.progreso + valor;
+  }
+  
 }
